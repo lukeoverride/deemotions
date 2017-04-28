e64a6352ec674f4140463dfeb3cc1a3ccce43dd8
lukeoverride <lucasurace11@gmail.com>
2017-04-28 12:05:50 +0200

    fixed and tested parser on pickle files

--- image_parser.py
+++ image_parser.py
@@ -59,10 +59,13 @@ def create_csv(input_path, label_path, output_path):
         if (len(splitted) == 4):
             noised = 1;
         #RETRIEVE HERE THE EMOTION FILE ASSOCIATED
-        emotion = int(np.loadtxt(label_path+"S"+person_id+"/"+seq_n+"/"+image_no_extension+"_emotion.txt", unpack=True))
+        emot = int(np.loadtxt(label_path+"S"+person_id+"/"+seq_n+"/"+image_no_extension+"_emotion.txt", unpack=True))
+        emoCode = list("0000000")
+        emoCode[emot-1]='1'
+        emotion = ''.join(emoCode)
         #Write the CSV file
         fd = open(input_path + '/prima_label.csv','a')
-        fd.write(image_path + "," + str(int(person_id)) + "," + str(int(seq_n)) + "," + str(emotion) + "," + str(int(noised)) + "\n")
+        fd.write(image_path + "," + str(int(person_id)) + "," + str(int(seq_n)) + "," + str(int(noised)) + "," + str(emotion) + "\n")
         fd.close()
         counter += 1
 
@@ -73,9 +76,11 @@ def create_csv(input_path, label_path, output_path):
 # single subject and a training matrix containing the images of all 
 # the other subjects.
 # @param csv_path the path to the CSV file generated with create_csv function
-# @param output_path the path where saving the 15 pickle files
+# @param output_path the path where saving the 118 pickle files
 # @param shuffle if True it randomises the position of the images in the training dataset
-def create_loo_pickle(csv_path, output_path, shuffle=False):
+def create_loo_pickle(csv_path, output_path):
+
+    if not os.path.exists(output_path): os.makedirs(output_path)
 
     #Saving the TEST file names in a list
     image_list = list()
@@ -89,8 +94,8 @@ def create_loo_pickle(csv_path, output_path, shuffle=False):
     #Loading the labels
     person_id_vector = np.genfromtxt(csv_path, delimiter=',', skip_header=1, usecols=(1), dtype=np.float32) #prende la seconda colonna (indice 1) come vettore
     seq_n_vector = np.genfromtxt(csv_path, delimiter=',', skip_header=1, usecols=(2), dtype=np.float32)
-    emotion_vector = np.genfromtxt(csv_path, delimiter=',', skip_header=1, usecols=(3), dtype=np.float32)
-    noised_vector = np.genfromtxt(csv_path, delimiter=',', skip_header=1, usecols=(4), dtype=np.float32)
+    noised_vector = np.genfromtxt(csv_path, delimiter=',', skip_header=1, usecols=(3), dtype=np.float32)
+    emotion_vector = np.genfromtxt(csv_path, delimiter=',', skip_header=1, usecols=(4), dtype=np.float32)
 
     #Printing shape
     print("Tot Images: " + str(len(image_list)))
@@ -105,15 +110,12 @@ def create_loo_pickle(csv_path, output_path, shuffle=False):
             #local variable clered at ech cycle
             training_list = list()
             training_emotion_list = list()
-            training_noised_list = list()
             
             valid_list = list()
             valid_emotion_list = list()
-            valid_noised_list = list()
 
             test_list = list()
             test_emotion_list = list()
-            test_noised_list = list()
             
             row_counter = 0
             insertedInTraining = 0
@@ -132,46 +134,44 @@ def create_loo_pickle(csv_path, output_path, shuffle=False):
                 if(int(person_id) == i and noised_vector[row_counter] == 0): #if it is not noised 
                      test_list.append(image)
                      test_emotion_list.append(emotion_vector[row_counter])
-                     test_noised_list.append(noised_vector[row_counter])
                 else:
                 #900 training set, 78 validation set, selected ramdomly
                      r = random.uniform(0,1)
                      if (r < 0.9 and insertedInTraining < 900):
                          training_list.append(image)         
                          training_emotion_list.append(emotion_vector[row_counter])
-                         training_noised_list.append(noised_vector[row_counter])
                          insertedInTraining += 1;
                      else:
                          valid_list.append(image)
                          valid_emotion_list.append(emotion_vector[row_counter])
-                         valid_noised_list.append(noised_vector[row_counter])
-                     #mi serve anche tenermi l'informazione sul numero di seq e noised value?
                 row_counter += 1
             
 
             #Create arrays
             training_array = np.asarray(training_list)
             training_emotion_array = np.asarray(training_emotion_list) 
-            training_noised_array = np.asarray(training_noised_list)
         
             test_array = np.asarray(test_list)
             test_emotion_array = np.asarray(test_emotion_list) 
-            test_noised_array = np.asarray(test_noised_list) 
+            
+            valid_array = np.asarray(valid_list)
+            valid_emotion_array = np.asarray(valid_emotion_list) 
 
             training_array = np.reshape(training_array, (-1, img_h*img_w*img_d))
             training_emotion_array = np.reshape(training_emotion_array, (-1, 1)) 
-            training_noised_array = np.reshape(training_noised_array, (-1, 1))
          
             test_array = np.reshape(test_array, (-1, img_h*img_w*img_d)) 
-            test_emotion_array = np.reshape(test_emotion_array, (-1, 1)) 
-            test_noised_array = np.reshape(test_noised_array, (-1, 1)) 
+            test_emotion_array = np.reshape(test_emotion_array, (-1, 1))
+            
+            valid_array = np.reshape(valid_array, (-1, img_h*img_w*img_d))
+            valid_emotion_array = np.reshape(valid_emotion_array, (-1, 1))
 
             print("Training dataset: ", training_array.shape)
             print("Training emotion label: ", training_emotion_array.shape)
-            print("Training noised label: ", training_emotion_array.shape)
+            print("Validation dataset: ", valid_array.shape)
+            print("Validation emotion label: ", valid_emotion_array.shape)
             print("Test dataset: ", test_array.shape)
             print("Test emotion label: ", test_emotion_array.shape)
-            print("Test noised label: ", test_noised_array.shape)
 
             #saving the dataset in a pickle file
             pickle_file = output_path + "/prima_p" + str(i) + "_out.pickle"
@@ -183,19 +183,18 @@ def create_loo_pickle(csv_path, output_path, shuffle=False):
                  save = {
                    'training_dataset': training_array,
                    'training_emotion_label': training_emotion_array,
-                   'training_noised_label': training_noised_array,    
                    'test_dataset': test_array,
                    'test_emotion_label': test_emotion_array,
-                   'test_noised_label': test_noised_array   
-                   # aggiungere salvataggio valid set 
+                   'validation_dataset': valid_array,
+                   'validation_emotion_label':valid_emotion_array 
                        }
 
                  print("Training dataset: ", training_array.shape)
                  print("Training emotion label: ", training_emotion_array.shape)
-                 print("Training noised label: ", training_noised_array.shape)
                  print("Test dataset: ", test_array.shape)
                  print("Test emotion label: ", test_emotion_array.shape)
-                 print("Test noised label: ", test_noised_array.shape)
+                 print("Validation dataset: ", valid_array.shape)
+                 print("Validation emotion label: ", valid_emotion_array.shape)
 
                  print("Saving the file...")
                  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
@@ -230,11 +229,9 @@ def show_pickle_element(pickle_file, element, element_type="training", img_size=
             handle = pickle.load(f)
             training_dataset = handle['training_dataset']
             training_emotion_label = handle['training_emotion_label']
-            training_noised_label = handle['training_noised_label']
             del handle  # hint to help gc free up memory
             print("Selected element: " + str(element))
             print("emotion: " + str(training_emotion_label[element]))
-            print("noised: " + str(training_noised_label[element]))
             print("")
             img = training_dataset[element]
             img_h, img_w, img_d = img.shape
@@ -248,11 +245,9 @@ def show_pickle_element(pickle_file, element, element_type="training", img_size=
             handle = pickle.load(f)
             test_dataset = handle['test_dataset']
             test_emotion_label = handle['test_emotion_label']
-            test_noised_label = handle['test_noised_label']
             del handle  # hint to help gc free up memory
             print("Selected element: " + str(element))
             print("emotion: " + str(test_emotion_label[element]))
-            print("noised: " + str(test_noised_label[element]))
             print("")
             img = test_dataset[element]
             img_h, img_w, img_d = image.shape
@@ -261,6 +256,22 @@ def show_pickle_element(pickle_file, element, element_type="training", img_size=
             #cv2.imshow('image',img)
             #cv2.waitKey(0)
             #cv2.destroyAllWindows()
+            
+    elif(element_type == "validation"):
+            handle = pickle.load(f)
+            valid_dataset = handle['validation_dataset']
+            valid_emotion_label = handle['validation_emotion_label']
+            del handle  # hint to help gc free up memory
+            print("Selected element: " + str(element))
+            print("emotion: " + str(valid_emotion_label[element]))
+            print("")
+            img = valid_dataset[element]
+            img_h, img_w, img_d = image.shape
+            img = np.reshape(img, (img_size,img_size,img_w))
+            cv2.imwrite( "./image.jpg", img );
+            #cv2.imshow('image',img)
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows() 
 
     else:
         raise ValueError('Error: element_type must be training or test.')
@@ -280,10 +291,10 @@ def main():
     create_csv(input_path="../EmotionDataset/data/ck/CK+/Blocks/face/", label_path="../EmotionDataset/data/ck/CK+/Emotion/", output_path="../EmotionDataset/data/ck/CK+/Blocks/face/")
 
 
-    #2- It creates 15 pickle files containing numpy arrays with images and labels.
+    #2- It creates 118 pickle files containing numpy arrays with images and labels.
     # You have to specify the CSV file path created in step 1.
 
-    #create_loo_pickle(csv_path="./prima_label.csv", output_path="./output", shuffle=False)
+    create_loo_pickle(csv_path="../EmotionDataset/data/ck/CK+/Blocks/face/prima_label.csv", output_path="./output")
 
 
     #3- You can check that everything is fine using this function.
