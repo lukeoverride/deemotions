49c8f56aff0396c5e0443ed7b75b480d495a0139
lukeoverride <lucasurace11@gmail.com>
2017-05-19 21:14:16 +0200

    GPL on drawLandmarks added

--- drawLandmarks.py
+++ drawLandmarks.py
@@ -1,5 +1,23 @@
 '''
-    Luca Surace, University of Calabria - Plymouth University
+    Copyright (C) 2017 Luca Surace - University of Calabria, Plymouth University
+    
+    This file is part of Deemotions. Deemotions is an Emotion Recognition System
+    based on Deep Learning method.
+
+    Deemotions is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Deemotions is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Deemotions.  If not, see <http://www.gnu.org/licenses/>.
+    
+    -----------------------------------------------------------------------
     
     This code projects the landmark points in the .txt files on the pictures (.png files) present in the
     directory. The path dir is taken by first input argument.
