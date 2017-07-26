fixed bugs: 1) scaling and mean on all faces instead of the current; 2) cnn_pred instead of cnn_pred.values()

wild/ClassifyImage.py
@@ -7,12 +7,11 @@ from GoogleDetector import GoogleDetector
 from ImagePreprocessing import ImagePreprocessing
 from bayesian_network import BayesianNetwork
 
-def classify_image(test_path, image_path,real_label):
+def classify_image(test_path, image_path,real_label, emotion_detector, google_detector, image_preprocessor, bayes_net):
 
     targets = ['Positive','Negative','Neutral']
+    reverse_index_list = [0, 2, 1]
 
-    google_detector = GoogleDetector()
-    image_preprocessor = ImagePreprocessing()
     labels = google_detector.detect_labels(image_path)
 
     with open(image_path, 'rb') as image:
@@ -21,19 +20,12 @@ def classify_image(test_path, image_path,real_label):
         image.seek(0)
         image_preprocessor.crop_faces(image_path, image, faces)
 
-    image_preprocessor.scale_images(test_path+"Faces/",64.0)
-    sess = tf.Session()
-    emotion_detector = CnnEmotionDetection(sess)
-    emotion_detector.load_variables('/home/napster/emotions/wild/checkpoints/emotiW_detection_171851/cnn_emotiW_detection-1499.meta',
-                                    '/home/napster/emotions/wild/checkpoints/emotiW_detection_171851/')
-    emotion_map = emotion_detector.getEmotionMap(test_path+"Scaled/")
+    image_preprocessor.scale_images(test_path+"Faces/",64.0, image_path)
+    emotion_map = emotion_detector.getEmotionMap(test_path+"Scaled/",image_path)
     cnn_predictions = emotion_detector.getMean(emotion_map)
 
-    my_bayes_net = BayesianNetwork()
-    is_correct = my_bayes_net.initModel("/home/napster/emotions/wild/wild_GAF_labels_histogram_train_global.csv",False)
-    print("Model correct: " + str(is_correct))
-    reverse_index_list = [0,2,1]
-    posterior = my_bayes_net.inferenceWithCNN(labels,reverse_index_list[np.argmax(cnn_predictions)])
+
+    posterior = bayes_net.inferenceWithCNN(labels,reverse_index_list[np.argmax(cnn_predictions.values())])
     final_predictions = np.argmax(posterior['emotion_node'].values)
     bayesian_label = targets[final_predictions]
 
@@ -45,7 +37,7 @@ def classify_image(test_path, image_path,real_label):
         print "*"
     else:
         print "Bayesian label: ", bayesian_label
-    print "CNN label: ",targets[reverse_index_list[np.argmax(cnn_predictions)]]
+    print "CNN label: ",targets[reverse_index_list[np.argmax(cnn_predictions.values())]]
     print labels
 
     return final_predictions
@@ -55,9 +47,21 @@ def classify_image(test_path, image_path,real_label):
 
 def main(test_path,real_label):
     os.chdir(test_path)
+
+    google_detector = GoogleDetector()
+    image_preprocessor = ImagePreprocessing()
+    sess = tf.Session()
+    emotion_detector = CnnEmotionDetection(sess)
+    emotion_detector.load_variables(
+        '/home/napster/emotions/wild/checkpoints/emotiW_detection_171851/cnn_emotiW_detection-1499.meta',
+        '/home/napster/emotions/wild/checkpoints/emotiW_detection_171851/')
+    my_bayes_net = BayesianNetwork()
+    is_correct = my_bayes_net.initModel("/home/napster/emotions/wild/wild_GAF_labels_histogram_train_global.csv", False)
+    print("Model correct: " + str(is_correct))
+
     predicted_counter = [0,0,0]
     for image_path in sorted(glob.glob("*")):
-        final_predictions = classify_image(test_path, image_path,real_label)
+        final_predictions = classify_image(test_path, image_path,real_label, emotion_detector, google_detector, image_preprocessor, my_bayes_net)
         predicted_counter[final_predictions] += 1
         print predicted_counter
         print ""

wild/cnn_wildemotion_detection.py
@@ -178,18 +178,20 @@ class CnnEmotionDetection:
         emotion_predictions = self._sess.run([self.cnn_yaw_output], feed_dict=feed_dict)
         return emotion_predictions
 
-    def getEmotionMap(self, input_path):
+    def getEmotionMap(self, input_path, image_path):
+        image_path_no_ext = image_path[0:len(image_path) - 4];
         emotion_all_faces = {}
-        for fileName in glob.glob(input_path + "*.jpg"):
-            image = cv2.imread(fileName).astype(np.float32)
-            current_pred = self.getEmotionsPredictions(image)
-            tokens = fileName.split("face")
-            completeFileName = tokens[0].split("/")
-            fileName = completeFileName[len(completeFileName) - 1]
-            if (fileName in emotion_all_faces):
-                emotion_all_faces[fileName] = np.append(emotion_all_faces[fileName], current_pred, axis=0)
-            else:
-                emotion_all_faces[fileName] = current_pred
+        for file in glob.glob(input_path + "*.jpg"):
+            if (image_path_no_ext in file):
+                image = cv2.imread(file).astype(np.float32)
+                current_pred = self.getEmotionsPredictions(image)
+                tokens = file.split("face")
+                completeFileName = tokens[0].split("/")
+                fileName = completeFileName[len(completeFileName) - 1]
+                if (fileName in emotion_all_faces):
+                    emotion_all_faces[fileName] = np.append(emotion_all_faces[fileName], current_pred, axis=0)
+                else:
+                    emotion_all_faces[fileName] = current_pred
         return emotion_all_faces
 
     def getMean(self, emotion_map):